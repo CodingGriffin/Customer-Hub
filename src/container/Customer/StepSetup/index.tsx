d361683678c8b702fed0c04264d81ad9e287c8a9
CodingGriffin <biggriffin640@gmail.com>
2025-02-27 13:30:40 -0800

    add get status fuction at container & add every status type

--- src/container/Customer/StepSetup/index.tsx
+++ src/container/Customer/StepSetup/index.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import StepSetup from '../../../page/Customer/OrdersTab/StepSetup';
 import { VersionsContext } from '../../../types';
+import actions from '../../../states/PADStatus/actions';
 
 export default function StepSetupContainer() {
   const { version_id } = useParams();
   const navigate = useNavigate();
-  const { selectedSection, setSelectedStep } = useOutletContext<VersionsContext>();
+  const dispatch = useDispatch();
+
+  const {
+    status,
+    loading,
+    error,
+  } = useSelector((state: any) => state.PADStatus);
+
+  const { selectedSection, selectedOrderData, setSelectedStep } = useOutletContext<VersionsContext>();
+
+  const padType = (selectedSection === 'data' ? 'data' : selectedSection === 'artwork' ? 'artw' : 'pack');
+  const pad_line_items_id = selectedOrderData?.pad_line_items?.find(
+    (item: any) => item.pad_abbreviation == padType && item.versions_id == version_id
+  )?.pad_line_items_id;
+  const job_number = selectedOrderData?.job?.job_number;
+
+  useEffect(() => {
+    if (pad_line_items_id) {
+      getStatus();
+    }
+    console.log("status=======>", status);
+  }, [pad_line_items_id]);
+
+  const getStatus = () => {
+    dispatch({
+      type: actions.GET_STATUS,
+      payload: {
+        mode: "getEventStatus",
+        pad_line_items_id: pad_line_items_id,
+        job_number: job_number
+      }
+    });
+  };
+
   const setStep = async (step: number) => {
     await setSelectedStep(step)
     console.log(version_id)
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -71,3 +71,16 @@ export interface VersionsContext {
   selectedSection: 'packaging' | 'artwork' | 'data' | 'shipments' | null;
   setSelectedStep: (step: number) => void;
 }
+
+export type status = {
+  setup: ['v-upload-nudge'],
+  upload: ['v-upload-wait', 'v-proof-rejected'],
+  proof: ['v-upload-receive', 'v-proof-sent', 'v-proof-nudge', 'v-production-photo-sample-rejected'],
+  photoSample: ['v-proof-approved', 'v-sample-sent', 'v-production-live-sample-rejected'],
+  liveSample: ['v-sample-sent', 'v-production-photo-sample-approved', 'v-production-live-sample-proof-sent']
+    
+    // v-production-nudge
+    // v-production-live-sample-approved
+    // v-production-proof-approved
+    // v-proof-rejected-production
+}
