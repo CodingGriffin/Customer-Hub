2424a0178e0a1e7c4be50071964db0a08578218e
CodingGriffin <biggriffin640@gmail.com>
2025-02-28 06:31:24 -0800

    add the updating status when moving from upload to proof

--- src/container/Customer/StepUpload/index.tsx
+++ src/container/Customer/StepUpload/index.tsx
@@ -3,9 +3,31 @@ import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
 
 import StepUpload from '../../../page/Customer/OrdersTab/StepUploads';
 import { VersionsContext } from '../../../types';
+import actions from "../../../states/PADStatus/actions";
+import { useDispatch } from 'react-redux';
 
 export default function StepUploadContainer() {
-  const { setStep } = useOutletContext<VersionsContext>();
+  const { setStep, currentStep, selectedOrderData } = useOutletContext<VersionsContext>();
+  const dispatch = useDispatch();
 
-  return <StepUpload setStep={setStep} />;
+  if (currentStep) {
+    setStep(currentStep);
+  }
+
+  const updateStatus = (pad_line_items_id: number) => {
+    dispatch({
+      type: actions.UPDATE_STATUS,
+      payload: {
+        mode: "proofSent",
+        pad_line_items_id: pad_line_items_id,
+        status: 1,
+        job_number: selectedOrderData.job.job_number,
+        code: "p",
+        abbr: "v-upload-receive",
+        onlyPhoto: false
+      }
+    });
+  }
+
+  return <StepUpload setStep={setStep} updateStatus={updateStatus} selectedOrderData={selectedOrderData} />;
 }
--- src/page/Customer/OrdersTab/StepUploads/index.tsx
+++ src/page/Customer/OrdersTab/StepUploads/index.tsx
@@ -5,19 +5,30 @@ import Nav from '../../../../component/StepUpload/NavComponent';
 import Advanced from './Advanced';
 import Review from './Review';
 import NameIcon from './NameIcon';
+import { useParams } from 'react-router-dom';
 
 interface StepUploadProps {
+  updateStatus: (pad_line_items_id: number) => void;
   setStep: (id: number) => void;
+  selectedOrderData: any
 }
 
-function StepUpload({setStep}: StepUploadProps) {
+function StepUpload({selectedOrderData, setStep, updateStatus}: StepUploadProps) {
+  const { version_id, section } = useParams();
   const [windowsDriveLabel, setWindowsDriveLabel] = useState('USB DRIVE');
   const [macosDriveLabel, setMacosDriveLabel] = useState('USB DRIVE');
   const [activeTab, setActiveTab] = useState('format');
   const [selectedFormat, setSelectedFormat] = useState('FAT32');
   const [nameIconStep, setNameIconStep] = useState(1);
 
+  const padType = section?.substring(0, 4);
+
+  const pad_line_items_id = selectedOrderData?.pad_line_items?.find(
+    (item: any) => item.pad_abbreviation == padType && item.versions_id == version_id
+  )?.pad_line_items_id;
+
   const continueSetup = () => {
+    updateStatus(pad_line_items_id);
     setStep(3);
   }
 
