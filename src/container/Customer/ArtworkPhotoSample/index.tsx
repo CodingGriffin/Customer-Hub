d08d84c48ed76d2ccb15479604d9dc51d26b75bc
CodingGriffin <biggriffin640@gmail.com>
2025-04-09 09:07:32 -0700

    update the photosample container of customer

--- src/container/Customer/ArtworkPhotoSample/index.tsx
+++ src/container/Customer/ArtworkPhotoSample/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-import ArtworkPhotoSample from '../../../page/Customer/OrdersTab/ArtworkPhotoSample';
 import { VersionsContext } from '../../../types';
+import actions from "../../../states/PhotoSamples/actions";
+import commentActions from "../../../states/Comments/actions";
+import ArtworkPhotoSample from '../../../page/Customer/OrdersTab/ArtworkPhotoSample';
 
 export default function ArtworkProof() {
-  const { version_id } = useParams();
+  const { version_id, section } = useParams();
   const navigate = useNavigate();
   const { selectedOrderData, selectedSection, setSelectedStep } = useOutletContext<VersionsContext>();
-  
+  const dispatch = useDispatch();
+
+  const {
+    samples,
+    loading,
+    error,
+  } = useSelector((state: any) => state.samples);
+
+  const {
+    comments,
+  } = useSelector((state: any) => state.comments);
+
   const setStep = async (step: number) => {
     await setSelectedStep(step)
     if (step === 5) {
@@ -19,6 +33,62 @@ export default function ArtworkProof() {
       }
     }
   };
+
+  useEffect(() => {
+    if (version_id) {
+      getPhotoSamples();
+    }
+  }, [dispatch, version_id, section]);
+
+  useEffect(() => {
+    if (samples.data && samples.data.length > 0) {
+      getComments();
+    }
+  }, [samples.data]);
+
+  const getPhotoSamples = () => {
+    const padType = (section === 'data' ? 'data' : section === 'artwork' ? 'artw' : 'pack');
+
+    dispatch({
+      type: actions.GET_SAMPLES,
+      payload: {
+        mode: "getPhotoSamples",
+        jobs_id: selectedOrderData.job.job_number,
+        versions_id: version_id,
+        pads_type: padType,
+      }
+    });
+  }
+
+  const addComment = (comment: string, sample_id: number) => {
+    dispatch({
+      type: commentActions.ADD_COMMENTS,
+      payload: {
+        mode: "insertSampleComment",
+        comment: comment,
+        resource_id: 100000 + sample_id,
+        table_code: "vendor_table"
+      }
+    });
+    getComments();
+  }
+
+  const getComments = () => {
+    // Extract all photo_sample_ids and add 100000 to each
+    const resourceIds = samples.data 
+      ? samples.data.map((sample: any) => 100000 + sample.photo_sample_id)
+      : [];
+
+    dispatch({
+      type: commentActions.GET_COMMENTS,
+      payload: {
+        mode: "getPhotoSampleComments",
+        resource_ids: '[' + resourceIds.join(',') + ']', // Convert array to comma-separated string
+      }
+    });
+  }
+
+  // return <PhotoSamples selectedOrderData={selectedOrderData} samples={samples.data ? samples.data : []} comments={comments.data ? comments.data : []} addComment={addComment} />
 
   return <ArtworkPhotoSample />;
 }
