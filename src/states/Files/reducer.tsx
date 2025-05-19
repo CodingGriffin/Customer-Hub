e34b9d13eef54808409a53b588d7b209bd0d7056
CodingGriffin <biggriffin640@gmail.com>
2025-05-19 08:32:42 -0700

    add the redux for revisions to Files state

--- src/states/Files/reducer.tsx
+++ src/states/Files/reducer.tsx
@@ -2,6 +2,7 @@ import actions from "./actions";
 
 const initialState = {
   files: {},
+  revisions: {},
   loading: false,
   error: null,
 };
@@ -80,7 +81,25 @@ function Reducer(state = initialState, action: any) {
         ...state,
         loading: false,
         error: action.payload
-      };  
+      };
+    case actions.GET_REVISIONS:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case actions.GET_REVISIONS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        revisions: action.payload,
+      };
+    case actions.GET_REVISIONS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
     default:
       return state;
   }
